5ff196de36a63c9c1f948850cd0340147e7a231d
Dou Kate <doukate@DoumatoMacBook-Pro-2.local>
2016-02-05 13:27:38 +0100

    have created "turn" method. and add the right rotation to balance the left rotation.

--- TestMaze.java
+++ TestMaze.java
@@ -10,28 +10,52 @@ public class TestMaze {
 		Findwall();
 	}
 
-	public static void Findwall() {
-		RobotConfig config = new RobotConfig();
-		RobotControl robot = new RobotControl(config);
-		robot.drive(400f, 400f);
-		while (config.getLeftMotor().isStalled() && config.getRightMotor().isStalled()) {
-			config.getLeftMotor().rotate(-10);
-			config.getRightMotor().rotate(-10);
+	
+	public static void turn(RobotConfig config, int angle) {
+		if (config.getLeftMotor().isStalled() && config.getRightMotor().isStalled()) {
+			config.getLeftMotor().rotate(angle);
+			config.getRightMotor().rotate(angle);
 
 			while (config.getLeftMotor().isMoving() && config.getRightMotor().isMoving()) {
 				config.getLeftMotor().forward();
 				config.getRightMotor().forward();
 			}
 
-			while (robot.getDistanceValue() <= 0.08) {
-				robot.drive(400f, 400f);
-				System.out.println(robot.getDistanceValue());
-//				Delay.msDelay(1000);
-			}
-//			while (robot.getDistanceValue() > 0.08) {
-//				robot.stopRobot();
+		}
+	}
+	
+	public static void Findwall() {
+		RobotConfig config = new RobotConfig();
+		RobotControl robot = new RobotControl(config);
+		// robot.drive(400f, 400f);
+
+		while (true) {
+			robot.drive(400f, 400f);
+//			while (robot.getDistanceValue() <= 0.08) {
+//				
+//				System.out.println(robot.getDistanceValue());
+//				// Delay.msDelay(1000);
 //			}
-			robot.stopRobot();
+			if (robot.getDistanceValue() > 0.08) {
+				robot.stopRobot();
+			}
+			
+			if (robot.getDistanceValue() <= 0.02) {
+				turn(config, 5);
+			}
+			
+			turn(config, -10);
+			
+//			if (config.getLeftMotor().isStalled() && config.getRightMotor().isStalled()) {
+//				config.getLeftMotor().rotate(-10);
+//				config.getRightMotor().rotate(-10);
+//
+//				while (config.getLeftMotor().isMoving() && config.getRightMotor().isMoving()) {
+//					config.getLeftMotor().forward();
+//					config.getRightMotor().forward();
+//				}
+//
+//			}
 		}
 	}
 
