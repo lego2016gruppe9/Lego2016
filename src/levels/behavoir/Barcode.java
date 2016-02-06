f45de7de2730fdff13896ef34715f0ff4dfe34fc
moshuixin <moshuixinxin@gmail.com>
2016-02-06 20:48:32 +0100

    test

--- src/levels/behavoir/Barcode.java
+++ src/levels/behavoir/Barcode.java
@@ -1,10 +1,8 @@
 package levels.behavoir;
 
-
 import Parcour.RobotConfig;
 import Parcour.RobotControl;
 import lejos.hardware.lcd.LCD;
-import lejos.hardware.sensor.EV3ColorSensor;
 
 public class Barcode {
 	RobotConfig config = new RobotConfig();
@@ -16,34 +14,30 @@ public class Barcode {
 	private final int BLACK = 0;
 	private final int WHITE = 1;
 	private int barcode = 0;
-	private  EV3ColorSensor colorSensor;
-	private final float THRESHOLD_WHITE = 0.7f;//The minimum color value of a white/silver element of the barcode
-	private final float WHEEL_LENGTH =  106.8142f;// the wheel-length value 需要测量
-	
-	
-	public void run(){
-		int position = BLACK;
-		long startTime = 0;
+	private final float THRESHOLD_WHITE = 0.7f;// The minimum color value of a white/silver element of the barcode
+	private final float WHEEL_LENGTH = 100.0f;// the wheel-length value 需要测量
+
+	public void run() {
+		int position = BLACK;// The position is black ground 
+		long startTime = 0;//Measures the time so that the length of the moved way can be calculated
 		boolean programRunning = true;
 		float currentColorValue = 0;
 	
-while (programRunning) {
-			
-			// Getting the current color value from the sensor
-			float[] sample = new float[colorSensor.sampleSize()];
-			colorSensor.fetchSample(sample, 0);
-			currentColorValue = sample[0];
-			
+
+		while (programRunning) {
+
+			currentColorValue = robot.getColorSensorValue();
+
 			// Displaying the current detected bar code
 			LCD.clear();
 			LCD.drawString("Barcode:", 0, 7);
 			LCD.drawInt(barcode, 3, "Barcode:".length(), 7);
-			//System.out.println("Barcode = " + barcode);
-			
+			// System.out.println("Barcode = " + barcode);
+
 			robot.drive(robot.maxSpeed() * 0.3f);
-			
-			if (currentColorValue > THRESHOLD_WHITE  ){
-				
+
+			if (currentColorValue > THRESHOLD_WHITE) {
+
 				if (position == BLACK) {
 					// Robot was previously on a black line
 					position = WHITE;
@@ -51,28 +45,32 @@ while (programRunning) {
 					barcode++;
 				}
 			} else {
-				
-				// Calculating the moved distance since the last white line
+
+				// Calculating the moved time since the last white line
 				float elapsedTime = System.nanoTime() - startTime;
 				float currentSpeed = robot.getSpeed();
-				float traveledDistanceDegree = currentSpeed * (elapsedTime / 1000000000.0f);	
-				float traveledDistanceMM = traveledDistanceDegree / 360.0f * WHEEL_LENGTH;
+				//calculating the radian 
+				float traveledDistanceDegree = currentSpeed * (elapsedTime / 1000000000.0f);// 计算弧度
+				// Calculating the moved distance
+				float traveledDistanceMM = traveledDistanceDegree / 360.0f * WHEEL_LENGTH;//计算弧长 = 前进的距离 
+				
+				
 
 				System.out.println("Distance = " + traveledDistanceMM);
-				
-				
-				if (position != BLACK){
+
+				if (position != BLACK) {
 					position = BLACK;
-				}else if( barcode >= 1 && traveledDistanceMM > WIDTH_BARCODE_ELEMENT) {
+				} else if (barcode >= 1
+						&& traveledDistanceMM > WIDTH_BARCODE_ELEMENT) {
 					programRunning = false;
 				}
-				}
-			if (barcode >7){
+			}
+			
+			if (barcode > 7) {
 				barcode = 0;
 				programRunning = false;
 			}
-			}
+		}
 	}
-	
 
 }
