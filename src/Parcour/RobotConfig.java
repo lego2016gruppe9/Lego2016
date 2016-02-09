f2c7d068fdcef4384959b3db771a79d3f6f83cb1
Yin Wang <friedrich.wang86@gmail.com>
2016-02-09 17:54:26 +0100

    public void moveInDistance(float speed,float distance){         if (speed > 0.0f){             //lMotorspeed = speed;             //rMotorspeed = speed;
                float TurnsOfTire = distance/(Tire_diameter * pi);
                float TurnsOfMotor = TurnsOfTire * 360.0f;
                 
                this.stopRobot();
                config.getLeftMotor().setSpeed(speed);
                config.getRightMotor().setSpeed(speed);
                 
                if (distance > 0.0f ){
                    config.getLeftMotor().rotate((int) TurnsOfMotor, true);
                    config.getRightMotor().rotate((int)TurnsOfMotor, true);
                }else if (distance < 0.0f){
                    config.getLeftMotor().rotate((int) ( TurnsOfMotor),
    true);
                    config.getRightMotor().rotate((int) ( TurnsOfMotor),
    true);
                }
                config.getLeftMotor().waitComplete();
                config.getRightMotor().waitComplete();
            }

--- src/Parcour/RobotConfig.java
+++ src/Parcour/RobotConfig.java
@@ -1,30 +1,32 @@
 package Parcour;
 
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class RobotConfig {
 
 	private final EV3LargeRegulatedMotor leftMotor;
 	private final EV3LargeRegulatedMotor rightMotor;
+	private final EV3MediumRegulatedMotor AncillaryMotor;
 	private final EV3ColorSensor colorSensor;
 	private  EV3UltrasonicSensor us;
-
 	private  EV3TouchSensor tleft;
 	private  EV3TouchSensor tright;
 
 	public RobotConfig() {
 		this.leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
 		this.rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+		this.AncillaryMotor = new EV3MediumRegulatedMotor(MotorPort.C);
 		this.colorSensor = new EV3ColorSensor(SensorPort.S4);
-		
-//		this.us = new EV3UltrasonicSensor(SensorPort.S2);
-//		this.tleft = new EV3TouchSensor(SensorPort.S3);
-	//	this.tright = new EV3TouchSensor(SensorPort.S1);
+		this.us = new EV3UltrasonicSensor(SensorPort.S2);
+		this.tleft = new EV3TouchSensor(SensorPort.S3);
+		this.tright = new EV3TouchSensor(SensorPort.S1);
 	}
 
 	/**
@@ -39,6 +41,10 @@ public class RobotConfig {
 	 */
 	public EV3LargeRegulatedMotor getRightMotor() {
 		return rightMotor;
+	}
+	
+	public EV3MediumRegulatedMotor getAncillaryMotor() {
+		return AncillaryMotor;
 	}
 
 	/**
